e0230abdd14376d68d8da07db84dcc3c808b34bc
Mathis.O <105793064+Mathou2000@users.noreply.github.com>
2022-06-21 22:50:03 +0200

    Add files via upload

---
+++ racine/E204_temp.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+   <meta charset="utf-8"> 
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <meta name="author" content="MO" />
+   <meta name="description" content="SAE23" />
+   <meta name="keywords" content="HTML, CSS" />
+   <title>Salle E204 </title>
+   <link rel="stylesheet" type="text/css" href="./Styles/styleRWD.css" />
+  
+ </head>
+ 
+ <body>
+  <header>
+    <h1> Capteur temperature  </h1>
+  </header>
+  <nav>
+    <ul>
+		<li><a href="./index.php"> Accueil </a></li>
+		<li><a href="./Gestion.php" > Gestion </a></li>
+		<li><a href="./Consultation.php"> Consultation </a></li>		
+	</ul>
+  </nav>
+  <section id="first">
+
+Les differents boutons sont pour supprimer le capteur, ajouter le capteur et vider les valeur du capteur
+<br>
+<br>
+<form>
+	<div class="form-example">
+    <input type="submit" value="supprimer">
+  </div>
+<br>
+	<div class="form-example">
+    <input type="submit" value="ajouter">
+  </div>
+<br>
+	<div class="form-example">
+    <input type="submit" value="vider">
+  </div>
+<br>
+</form>
+
+
+  </section>
+  
+
+  <footer>
+    <ul>
+	  <li>IUT de Blagnac</li>
+	  <li>Département Réseaux et Télécommunications</li>
+      <li>BUT1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+	</ul>  
+  </footer>
+ </body>
+</html>
